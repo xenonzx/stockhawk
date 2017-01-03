bf5bf5e998764b13401f8de91f4a8a8a02589a98
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2017-01-03 13:31:40 +0200

    adding StockWidgetProvider.java

---
+++ app/src/main/java/com/udacity/stockhawk/StockWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.udacity.stockhawk;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by ahmed on 03/01/17.
+ */
+
+public class StockWidgetProvider extends AppWidgetProvider {
+}
