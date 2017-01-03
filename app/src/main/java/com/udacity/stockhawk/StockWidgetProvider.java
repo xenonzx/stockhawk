d74ecd41af76142e97e808d2d5cd8ebd03ce1de8
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2017-01-03 14:04:56 +0200

    creating on update method to update remote views

--- app/src/main/java/com/udacity/stockhawk/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/StockWidgetProvider.java
@@ -1,10 +1,22 @@
 package com.udacity.stockhawk;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
 
 /**
  * Created by ahmed on 03/01/17.
  */
 
 public class StockWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // once on update is called will make remote views object and notify all widgets with the view through passed id -through using app widget manager-
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], views);
+
+        }
+    }
 }
