ecdf111633d21f894b2b6371b8c71aceba2b3437
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2017-01-04 08:10:07 +0200

    starting intent service to update the widget on receiving update data broadcast

--- app/src/main/java/com/udacity/stockhawk/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/StockWidgetProvider.java
@@ -1,23 +1,19 @@
 package com.udacity.stockhawk;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.widget.RemoteViews;
 
 import com.udacity.stockhawk.sync.QuoteSyncJob;
-import com.udacity.stockhawk.ui.MainActivity;
 
 /**
  * Created by ahmed on 03/01/17.
  */
 
 public class StockWidgetProvider extends AppWidgetProvider {
-    int REQUEST_CODE = 0;
-    int NO_FLAGS = 0;
+
     String TAG = StockWidgetProvider.class.getSimpleName();
 
     @Override
@@ -25,7 +21,7 @@ public class StockWidgetProvider extends AppWidgetProvider {
         super.onReceive(context, intent);
         Log.v(TAG, "onReceive" + intent.getAction());
         if (intent.getAction().equals(QuoteSyncJob.ACTION_DATA_UPDATED)) {
-            // TODO:  start service to query and update widget
+            context.startService(new Intent(context, WidgetIntentService.class));
         }
     }
 
@@ -33,17 +29,8 @@ public class StockWidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         Log.v(TAG, "onUpdate");
         // once on update is called will make remote views object and notify all widgets with the view through passed id -through using app widget manager-
-        for (int i = 0; i < appWidgetIds.length; i++) {
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
-            views.setOnClickPendingIntent(R.id.tv_widget_header, getMainActivityIntent(context));
-            appWidgetManager.updateAppWidget(appWidgetIds[i], views);
 
-        }
-    }
-
-    PendingIntent getMainActivityIntent(Context cxt) {
-        Intent i = new Intent(cxt, MainActivity.class);
-        PendingIntent pi = PendingIntent.getActivity(cxt, REQUEST_CODE, i, NO_FLAGS);
-        return pi;
     }
 }
+
+
